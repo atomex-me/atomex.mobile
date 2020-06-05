1750c95349d65823bca7b7d5b80dc1b38f6a8aff
mismirnov <mi.smirnov24@gmail.com>
2020-06-06 01:22:24 +0300

    android custom entry

---
+++ atomex.Android/CustomElements/CustomEntryRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Android.Content;
+using Android.Text;
+using atomex.CustomElements;
+using atomex.Droid.CustomElements;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace atomex.Droid.CustomElements
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+            AutoPackage = false;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.SetPadding(60, -10, 60, 0);
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+                this.Control.SetRawInputType(InputTypes.TextFlagNoSuggestions);
+            }
+        }
+    }
+}
+
