a715f219880b495024b77b9d634d179c68bd9569
mismirnov <mi.smirnov24@gmail.com>
2020-07-01 19:44:59 +0300

    android local notification

---
+++ atomex.Android/AndroidNotificationManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using atomex.Services;
+using Android.Support.V4.App;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using atomex.Models;
+using Android.OS;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(atomex.Droid.AndroidNotificationManager))]
+namespace atomex.Droid
+{
+    public class AndroidNotificationManager : INotificationManager
+    {
+        const string channelId = "default";
+        const string channelName = "Default";
+        const string channelDescription = "The default channel for notifications.";
+        const int pendingIntentId = 0;
+
+        public const string TitleKey = "title";
+        public const string MessageKey = "message";
+
+        bool channelInitialized = false;
+        int messageId = -1;
+        NotificationManager manager;
+
+        public event EventHandler NotificationReceived;
+
+        public void Initialize()
+        {
+            CreateNotificationChannel();
+        }
+
+        public int ScheduleNotification(string title, string message)
+        {
+            if (!channelInitialized)
+            {
+                CreateNotificationChannel();
+            }
+
+            messageId++;
+
+            Intent intent = new Intent(Android.App.Application.Context, typeof(MainActivity));
+            intent.PutExtra(TitleKey, title);
+            intent.PutExtra(MessageKey, message);
+
+            PendingIntent pendingIntent = PendingIntent.GetActivity(Android.App.Application.Context, pendingIntentId, intent, PendingIntentFlags.OneShot);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(Android.App.Application.Context, channelId)
+                .SetContentIntent(pendingIntent)
+                .SetContentTitle(title)
+                .SetContentText(message)
+                .SetLargeIcon(BitmapFactory.DecodeResource(Android.App.Application.Context.Resources, Resource.Drawable.AppIcon))
+                .SetSmallIcon(Resource.Drawable.AppIcon)
+                .SetDefaults((int)NotificationDefaults.Sound | (int)NotificationDefaults.Vibrate);
+
+            var notification = builder.Build();
+            manager.Notify(messageId, notification);
+
+            return messageId;
+        }
+
+        public void ReceiveNotification(string title, string message)
+        {
+            var args = new NotificationEventArgs()
+            {
+                Title = title,
+                Message = message,
+            };
+            NotificationReceived?.Invoke(null, args);
+        }
+
+        void CreateNotificationChannel()
+        {
+            manager = (NotificationManager)Android.App.Application.Context.GetSystemService(Android.App.Application.NotificationService);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                var channelNameJava = new Java.Lang.String(channelName);
+                var channel = new NotificationChannel(channelId, channelNameJava, NotificationImportance.Default)
+                {
+                    Description = channelDescription
+                };
+                manager.CreateNotificationChannel(channel);
+            }
+
+            channelInitialized = true;
+        }
+
+        public void RemoveNotifications()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- atomex.Android/MainActivity.cs
+++ atomex.Android/MainActivity.cs
@@ -2,10 +2,13 @@
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Content;
+using Xamarin.Forms;
+using atomex.Services;
 
 namespace atomex.Droid
 {
-    [Activity(Label = "atomex", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "atomex", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, LaunchMode = LaunchMode.SingleTop)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -18,12 +21,26 @@ namespace atomex.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            CreateNotificationFromIntent(Intent);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        protected override void OnNewIntent(Intent intent)
+        {
+            CreateNotificationFromIntent(intent);
+        }
+        void CreateNotificationFromIntent(Intent intent)
+        {
+            if (intent?.Extras != null)
+            {
+                string title = intent.Extras.GetString(AndroidNotificationManager.TitleKey);
+                string message = intent.Extras.GetString(AndroidNotificationManager.MessageKey);
+                DependencyService.Get<INotificationManager>().ReceiveNotification(title, message);
+            }
         }
     }
 }
