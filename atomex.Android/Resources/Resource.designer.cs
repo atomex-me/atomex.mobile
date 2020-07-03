f59e9184b06b71d3bf6901b3f7edb66092779939
mismirnov <mi.smirnov24@gmail.com>
2020-07-03 21:01:00 +0300

    few layout fixes

--- atomex.Android/Resources/Resource.designer.cs
+++ atomex.Android/Resources/Resource.designer.cs
@@ -8451,31 +8451,34 @@ namespace atomex.Droid
 			public const int SwapPayment = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int SwapRefund = 2131165330;
+			public const int SwapRedeem = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int Tick = 2131165331;
+			public const int SwapRefund = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int tooltip_frame_dark = 2131165332;
+			public const int Tick = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int tooltip_frame_light = 2131165333;
+			public const int tooltip_frame_dark = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int TZBTC = 2131165334;
+			public const int tooltip_frame_light = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int USDT = 2131165335;
+			public const int TZBTC = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int Wallet__blue = 2131165336;
+			public const int USDT = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int Wallet__white = 2131165337;
+			public const int Wallet__blue = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int XTZ = 2131165338;
+			public const int Wallet__white = 2131165338;
+			
+			// aapt resource value: 0x7F07009B
+			public const int XTZ = 2131165339;
 			
 			static Drawable()
 			{
