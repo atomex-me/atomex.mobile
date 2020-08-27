7c99206c56a25a213c4665d155ebbf0fb8588f87
mismirnov <mi.smirnov24@gmail.com>
2020-08-27 19:49:20 +0300

    android camera permissions fixed

--- atomex.Android/MainActivity.cs
+++ atomex.Android/MainActivity.cs
@@ -48,6 +48,7 @@ namespace atomex.Droid
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
