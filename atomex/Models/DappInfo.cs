4c514d42115d979a62c290fb7299dfe3eb35f774
Sergey Bokoch <bokoch98@yandex.ru>
2021-10-28 06:38:49 +0300

    expandable adding mock permissions

--- atomex/Models/DappInfo.cs
+++ atomex/Models/DappInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Atomex.Core;
 using Microsoft.Extensions.FileProviders;
 
@@ -16,10 +17,14 @@ namespace atomex.ViewModel
         public DappType DappDeviceType { get; set; }
         public IReadOnlyCollection<Permission> Permissions { get; set; }
 
+        public string PermissionsFormatted => Permissions != null
+            ? string.Join(", ", Permissions.Select(x => x.Name)) 
+            : string.Empty;
+
         public static List<DappInfo> MockDapps() =>
             new List<DappInfo>()
             {
-                new() { Name = "abcd", Network = Network.TestNet, ImageUrl = "LTC", IsActive = true, DappDeviceType = DappType.Desktop},
+                new() { Name = "abcd", Network = Network.TestNet, ImageUrl = "LTC", IsActive = true, DappDeviceType = DappType.Desktop, Permissions = new List<Permission>() { new Permission() {Name="Ты можешь читать"}, new Permission() {Name="Ты можешь писать"}, new Permission() {Name="Ты не можешь отсосать"}}},
                 new() { Name = "xyz", Network = Network.MainNet, ImageUrl = "BTC", IsActive = true, DappDeviceType = DappType.Mobile},
                 new() { Name = "Desktop", Network = Network.MainNet, ImageUrl = "ETH", IsActive = true, DappDeviceType = DappType.Web},
                 new() { Name = "xyz5", Network = Network.MainNet, ImageUrl = "LTC", IsActive = true, DappDeviceType = DappType.Mobile},
@@ -29,7 +34,7 @@ namespace atomex.ViewModel
                 new() { Name = "xyz43", Network = Network.MainNet,ImageUrl = "LTC", IsActive = true, DappDeviceType = DappType.Mobile},
             };
     }
-    
+
     public enum DappType
     {
         Mobile,
