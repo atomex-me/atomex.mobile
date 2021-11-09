2212802f604991cae32623361c475b647de828cc
Mikhail Smirnov <58223117+mismirnov@users.noreply.github.com>
2021-11-09 18:47:50 +0300

    Fix toast for android API 30+ (#49)

--- atomex.Android/Services/ToastService.cs
+++ atomex.Android/Services/ToastService.cs
@@ -2,6 +2,8 @@
 using Android.Content;
 using Xamarin.Forms;
 using Android.Widget;
+using System;
+using Serilog;
 
 [assembly: Dependency(typeof(atomex.Droid.Services.ToastService))]
 namespace atomex.Droid.Services
@@ -10,35 +12,46 @@ namespace atomex.Droid.Services
     {
         public void Show(string message, ToastPosition toastPosition, string appTheme)
         {
-            Context context = Android.App.Application.Context;
-            Toast toast = Toast.MakeText(context, message, ToastLength.Short);
-            toast.View.SetBackgroundResource(Resource.Drawable.CustomToast);
-            toast.SetGravity(Android.Views.GravityFlags.Center, 0, 0);
-            TextView tv = toast.View.FindViewById<TextView>(Android.Resource.Id.Message);
-            tv.Gravity = Android.Views.GravityFlags.CenterVertical | Android.Views.GravityFlags.CenterHorizontal;
-            tv.SetPadding(20, 0, 20, 0);
-
-            if (appTheme == "Dark")
-                tv.SetTextColor(Android.Graphics.Color.Black);
-            else
-                tv.SetTextColor(Android.Graphics.Color.White);
-
-            switch (toastPosition)
+            try
             {
-                case ToastPosition.Top:
-                    toast.SetGravity(Android.Views.GravityFlags.Top, 0, 200);
-                    break;
-                case ToastPosition.Center:
-                    toast.SetGravity(Android.Views.GravityFlags.CenterVertical, 0, 0);
-                    break;
-                case ToastPosition.Bottom:
-                    toast.SetGravity(Android.Views.GravityFlags.Bottom, 0, 200);
-                    break;
-                default:
-                    toast.SetGravity(Android.Views.GravityFlags.Top, 0, 200);
-                    break;
+                Context context = Android.App.Application.Context;
+                Toast toast = Toast.MakeText(context, message, ToastLength.Short);
+
+                if (Xamarin.Essentials.DeviceInfo.Version.Major <= 10)
+                {
+                    toast.View.SetBackgroundResource(Resource.Drawable.CustomToast);
+                    toast.SetGravity(Android.Views.GravityFlags.Center, 0, 0);
+                    TextView tv = toast.View.FindViewById<TextView>(Android.Resource.Id.Message);
+                    tv.Gravity = Android.Views.GravityFlags.CenterVertical | Android.Views.GravityFlags.CenterHorizontal;
+                    tv.SetPadding(20, 0, 20, 0);
+
+                    if (appTheme == "Dark")
+                        tv.SetTextColor(Android.Graphics.Color.Black);
+                    else
+                        tv.SetTextColor(Android.Graphics.Color.White);
+                }
+
+                switch (toastPosition)
+                {
+                    case ToastPosition.Top:
+                        toast.SetGravity(Android.Views.GravityFlags.Top, 0, 200);
+                        break;
+                    case ToastPosition.Center:
+                        toast.SetGravity(Android.Views.GravityFlags.CenterVertical, 0, 0);
+                        break;
+                    case ToastPosition.Bottom:
+                        toast.SetGravity(Android.Views.GravityFlags.Bottom, 0, 200);
+                        break;
+                    default:
+                        toast.SetGravity(Android.Views.GravityFlags.Top, 0, 200);
+                        break;
+                }
+                toast.Show();
             }
-            toast.Show();
+            catch (Exception e)
+            {
+                Log.Error(e, "Show toast error");
+            }
         }
     }
 }
