fa50b6da57c49b6ac3697ca6a9531e19461d9eda
iviich4eL <Mich4el@protonmail.com>
2022-03-18 13:31:32 +0300

    ConfirmDappPage

---
+++ atomex/ViewModel/WalletBeacon/ConfirmDappViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Beacon.Sdk.Beacon;
+using Xamarin.Forms;
+
+namespace atomex.ViewModel.WalletBeacon
+{
+    public class ConfirmDappViewModel : BaseViewModel
+    {
+        public INavigation Navigation { get; }
+
+        public P2PPairingRequest PairingRequest { get; }
+
+        public ConfirmDappViewModel(INavigation navigation, P2PPairingRequest pairingRequest)
+        {
+            Navigation = navigation ?? throw new ArgumentNullException(nameof(navigation));
+            PairingRequest = pairingRequest ?? throw new ArgumentNullException(nameof(pairingRequest));
+
+            ConnectCommand = new Command(async () => await ConnectAsync());
+            CancelCommand = new Command(async () => await CancelAsync());
+        }
+
+        public ICommand ConnectCommand { get; }
+        private async Task ConnectAsync()
+        {
+
+        }
+
+        public ICommand CancelCommand { get; }
+        private async Task CancelAsync() => await Navigation.PopAsync();
+
+
+        //void LoadDapp()
+        //{
+        //    Dapp = new DappInfo()
+        //    {
+        //        Name = "Dapp",
+        //        Network = Network.TestNet,
+        //        ImageUrl = "BTC",
+        //        DappDeviceType = DappType.Mobile,
+        //        Permissions = new List<Permission>()
+        //        {
+        //            new Permission() { Name = "Ты можешь сидеть?" },
+        //            new Permission() { Name = "Тебе еще что-то надо?" }
+        //        }
+        //    };
+        //}
+    }
+}
---
+++ atomex/Views/WalletBeacon/ConfirmDappPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using atomex.ViewModel.WalletBeacon;
+using Xamarin.Forms;
+
+namespace atomex.Views.WalletBeacon
+{
+    public partial class ConfirmDappPage : ContentPage
+    {
+        public ConfirmDappPage()
+        {
+            InitializeComponent();
+        }
+
+        public ConfirmDappPage(ConfirmDappViewModel dappViewModel)
+        {
+            InitializeComponent();
+            BindingContext = dappViewModel;
+        }
+    }
+}
