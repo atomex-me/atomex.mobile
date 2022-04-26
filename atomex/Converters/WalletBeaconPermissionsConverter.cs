8480c21df2045f7780f4da12dbf246427d5789df
iviich4eL <Mich4el@protonmail.com>
2022-04-26 20:46:44 +0300

    Create WalletBeaconPermissionsConverter.cs

---
+++ atomex/Converters/WalletBeaconPermissionsConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Beacon.Sdk.Beacon.Permission;
+using Xamarin.Forms;
+
+namespace atomex.Converters
+{
+    public class WalletBeaconPermissionsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var permissions = (List<PermissionScope>) value;
+            var result = "Permissions: ";
+
+            if (permissions.Count > 0)
+                foreach (var permission in permissions)
+                    result += permission.ToString() + ' ';
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
