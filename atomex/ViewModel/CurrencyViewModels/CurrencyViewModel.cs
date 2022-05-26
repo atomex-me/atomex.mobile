cbfb10b897ddda32ee2a253bbc69474bb8e4c1ca
mismirnov <mi.smirnov24@gmail.com>
2022-05-26 18:32:21 +0300

    Android icons, few layout changes

--- atomex/ViewModel/CurrencyViewModels/CurrencyViewModel.cs
+++ atomex/ViewModel/CurrencyViewModels/CurrencyViewModel.cs
@@ -60,8 +60,8 @@ namespace atomex.ViewModel.CurrencyViewModels
         public string FeeCurrencyCode => Currency.FeeCode;
         public string BaseCurrencyCode => "USD";
         public bool IsBitcoinBased => Currency is BitcoinBasedConfig;
-        public bool IsStakingAvailable => Currency is TezosConfig;
-        public bool HasCollectibles => Currency is TezosConfig;
+        public bool IsStakingAvailable => Currency.Name == "XTZ";
+        public bool HasCollectibles => Currency.Name == "XTZ";
 
         [Reactive] public decimal TotalAmount { get; set; }
         [Reactive] public decimal TotalAmountInBase { get; set; }
