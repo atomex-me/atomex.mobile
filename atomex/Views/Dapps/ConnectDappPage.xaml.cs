04043ff618b08a2c9f5bed25cb0bbe9da4fda5fd
mismirnov <mi.smirnov24@gmail.com>
2022-12-27 17:42:55 +0300

    Layout changes, added scanner overlay to connect dApp

--- atomex/Views/Dapps/ConnectDappPage.xaml.cs
+++ atomex/Views/Dapps/ConnectDappPage.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using atomex.ViewModels.DappsViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,6 +12,14 @@ namespace atomex.Views.Dapps
         {
             InitializeComponent();
             BindingContext = connectDappViewModel;
+        }
+        private void DisableScannerOverlay(object sender, EventArgs e)
+        {
+            Overlay.IsVisible = false;
+        }
+        private void EnableScannerOverlay(object sender, EventArgs e)
+        {
+            Overlay.IsVisible = true;
         }
     }
 }
