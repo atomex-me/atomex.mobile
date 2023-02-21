4ae0f44182a4b3bbc0d17dbd1ad51dc0a58ba1a4
mismirnov <mi.smirnov24@gmail.com>
2023-02-21 16:32:50 +0300

    Increase Beacon.Sdk version, libsodium removed

--- atomex.iOS/Main.cs
+++ atomex.iOS/Main.cs
@@ -1,5 +1,4 @@
-﻿using Beacon.Sdk.Core.Infrastructure.Cryptography.Libsodium;
-using UIKit;
+﻿using UIKit;
 
 namespace atomex.iOS
 {
@@ -8,7 +7,6 @@ namespace atomex.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            Sodium.SetLibraryType(SodiumLibraryType.StaticInternal);
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
